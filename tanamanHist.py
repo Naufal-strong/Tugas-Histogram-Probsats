0bb49deb6d80415c2d98e71bbd225e85b9a6a802
Naufal-strong <naufalfakhrianto20@gmail.com>
2025-10-26 23:05:27 +0700

    tanamanHist.py

--- tanamanHist.py
+++ tanamanHist.py
@@ -35,6 +35,28 @@ print(f"Standar Deviasi : {stddeviasi_produksi:.2f}")
 
 plt.hist(values, bins=bins, color='lightyellow', edgecolor='black')
 
+counts, bin_edges = np.histogram(values, bins=bins)
+
+kde = gaussian_kde(values)
+bin_width = bins[1] - bins[0]
+x_grid = np.linspace(min(values), max(values), 1000)
+kde_density = kde(x_grid)
+
+kde_frequency = kde_density * len(values) * bin_width
+
+max_hist_height = np.max(counts)
+
+max_kde_height = np.max(kde_frequency)
+
+if max_kde_height > 0:
+    scale_factor = max_hist_height / max_kde_height
+    kde_frequency_scaled = kde_frequency * scale_factor
+else:
+    kde_frequency_scaled = kde_frequency
+
+plt.plot(x_grid, kde_frequency_scaled, color="blue", linewidth=2, label="Kurva (KDE)")
+
+
 plt.axvline(mean_produksi, color='red', linestyle='--', label='Mean')
 plt.axvline(median_produksi, color='green', linestyle='--', label='Median')
 plt.axvline(modus_produksi, color='orange', linestyle='--', label='Mode')
@@ -47,5 +69,3 @@ plt.xticks(np.arange(0, values.max() + 1000, 1000))
 plt.tick_params(axis='x', rotation=45)
 plt.legend()
 plt.show()
-
-
